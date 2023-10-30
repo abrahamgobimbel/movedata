413ca5c20a3bef763432755a9c0fa07656bac590
abrahamgobimbel <abraham9haan.go@gmail.com>
2023-10-30 15:24:32 +0700

    .

--- functionsql.py
+++ functionsql.py
@@ -1,37 +1,36 @@
 import mysql.connector
 import datetime
-import db_materi
-import db_kbm
+import db_materi  # Ensure db_materi contains the necessary query_select function
+import db_kbm  # Ensure db_kbm contains the necessary query_select function
 
 def format_datetime(dt):
     return dt.strftime("%Y-%m-%d %H:%M:%S")
 
 def data_sql(nama_database, sumber):
-    
-    connection = mysql.connector.connect(
-        host     = "localhost",
-        user     = "root",
-        password = "gobimbel",
-        database = nama_database  
-    )
-    cursor = connection.cursor()
+    try:
+        connection = mysql.connector.connect(
+            host="localhost",
+            user="root",
+            password="gobimbel",
+            database=nama_database
+        )
+        cursor = connection.cursor()
 
-    if nama_database == 'db_materi' :
-        query = db_materi.query_select(sumber)
-    elif nama_database == 'db_kbm' :
-        query = db_kbm.query_select(sumber)
-    
-    cursor.execute(query)
-    results = cursor.fetchall()
-    
-    data_sql = []
-    for row in results:
-        for index, value in enumerate(row):
-            if isinstance(value, datetime.datetime):
-                row = list(row)  
-                row[index] = format_datetime(value)
-                data_sql.append(row)
-    
-    cursor.close()
-    connection.close()
-    return data_sql
+        if nama_database == 'db_materi':
+            query = db_materi.query_select(sumber)
+        elif nama_database == 'db_kbm':
+            query = db_kbm.query_select(sumber)
+
+        cursor.execute(query)
+        results = cursor.fetchall()
+        data_sql = []
+        for row in results:
+            formatted_row = [format_datetime(value) if isinstance(value, datetime.datetime) else value for value in row]
+            data_sql.append(formatted_row)
+        return data_sql
+
+    except mysql.connector.Error as err:
+        print(f"Error: {err}")
+    finally:
+        cursor.close()
+        connection.close()
--- template_query.py
+++ template_query.py
@@ -131,7 +131,6 @@ values = []
 for array in data_sql:
     new_tuple = tuple(function.format_datetime(val) for val in array)
     values.append(new_tuple)
-modified_values = [(t[0], t[1].replace("'", ''), t[2]) for t in values]
 if len(values) > 10 :
     batch_size = round(len(values)/10)
 else : 
