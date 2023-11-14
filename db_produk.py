05f179511dc1ca35d59378baa6bd93eabce2aa41
abrahamgobimbel <abraham9haan.go@gmail.com>
2023-11-14 13:17:58 +0700

    .

--- db_produk.py
+++ db_produk.py
@@ -8,7 +8,8 @@ def query_select(nama_table) :
         file_path_bundling = 'db_produk_bundling.txt'
         data_bundling = functioninput.data_input(file_path_bundling)
         query = f"SELECT DISTINCT tb.* FROM db_GOIconsV2.t_Bundling tb WHERE c_IdBundling IN ({', '.join(data_bundling)});"
-        
+        if data_bundling == [] :
+            query = f"SELECT DISTINCT tb.* FROM db_GOIconsV2.t_Bundling tb;"
     
     elif nama_table == 't_isi_tob' : 
         file_path_tob = 'db_produk_tob.txt'
@@ -20,11 +21,11 @@ def query_select(nama_table) :
                  f"FROM db_GOIconsV2.t_IsiProdukMix tipm\n")
     
     elif nama_table == 't_jenis_kelas' : 
-        query = f"SELECT * FROM db_produk.t_mkt_jeniskelas;"
+        query = f"SELECT * FROM db_GOIconsV2.t_MKT_JenisKelas;"
     
     
     elif nama_table == 't_jenis_produk' :
-        query = f"SELECT *, current_timestamp FROM db_produk.t_mkt_jenisproduk;"
+        query = f"SELECT *, current_timestamp FROM db_GOIconsV2.t_MKT_JenisProduk;"
     
     
     elif nama_table == 't_paket_dan_bundel' :
@@ -41,6 +42,9 @@ def query_select(nama_table) :
                  f"from db_banksoalV2.t_PaketSoal tps \n"
                  f"JOIN db_banksoalV2.t_IsiTOB tit ON tit.c_KodePaket = tps.c_KodePaket\n"
                  f"WHERE tit.c_KodeTOB IN ({', '.join(data_tob)});") 
+        if data_tob == [] :
+            query = (f"SELECT DISTINCT {', '.join(unique_select)}\n"
+                 f"from db_banksoalV2.t_PaketSoal tps \n")
     
     
     elif nama_table == 't_produk' :
@@ -61,10 +65,10 @@ def query_select(nama_table) :
                     f"FROM db_GOIconsV2.t_Produk tp\n"
                     f"JOIN db_GOIconsV2.t_IsiProdukMix tispm USING (c_IdProduk)"
                     f"WHERE tispm.c_IdProdukMix IN ({', '.join(data_produk_mix)});")
-            
-        
-        
-    
+            if data_produk_mix == [] : 
+                query = (f"SELECT DISTINCT tp.*\n"
+                        f"FROM db_GOIconsV2.t_Produk tp\n")
+                
     elif nama_table == 't_produk_mix' :
         unique_select = ['tpm.c_IdProdukMix' , 'tpm.c_NamaProdukMix' , 'tpm.c_IdJenisKelas' , 'tpm.c_IdKurikulum' , 
                         'tpm.c_IdSekolahKelas' ,' tpm.c_IsKerjaSama' , 'tpm.c_IsJenisPertemuan' , 'tpm.c_JumS1' , 
@@ -82,6 +86,9 @@ def query_select(nama_table) :
                     f"FROM db_GOIconsV2.t_ProdukMix tpm\n"
                     f"JOIN db_GOIconsV2.t_Bundling tb USING (c_IdProdukMix)"
                     f"WHERE tb.c_IdBundling IN ({', '.join(data_bundling)});")
+            if data_bundling == [] :
+                query = (f"SELECT {', '.join(unique_select)}\n"
+                    f"FROM db_GOIconsV2.t_ProdukMix tpm\n")
 
         
         
@@ -92,9 +99,9 @@ def query_select(nama_table) :
     
     
     elif nama_table == 't_tob' : 
-        unique_select = ["tt.c_KodeTOB" , "c_NamaTOB" , "c_JarakAntarPaket" , "'2023-11-11 00:00:01' as c_TanggalAktif", 
-                         "'2023-11-11 16:59:00' as c_TanggalKadaluarsa","tt.c_TahunAjaran" , "tt.c_Jenis" , "tt.c_IsTOMerdeka" ,
-                         "'Aktif'  as c_Status" , "tt.c_Updater" , "tt.c_LastUpdate" , "CURRENT_TIMESTAMP()" ]
+        unique_select = ["tt.c_KodeTOB" , "c_NamaTOB" , "c_JarakAntarPaket" , "c_TanggalAktif", 
+                         "c_TanggalKadaluarsa","tt.c_TahunAjaran" , "tt.c_Jenis" , "tt.c_IsTOMerdeka" ,
+                         "c_Status" , "tt.c_Updater" , "tt.c_LastUpdate" , "CURRENT_TIMESTAMP()" ]
         file_path_tob = 'db_produk_kode_tob.txt'
         data_tob = functioninput.data_input(file_path_tob)
         query = (f"SELECT DISTINCT {', '.join(unique_select)}\n"
@@ -107,6 +114,13 @@ def query_select(nama_table) :
                  f"FROM db_banksoalV2.t_TOB tt\n"
                  f"JOIN db_banksoalV2.t_ProdukTOB tpt ON tt.c_KodeTOB = tpt.c_KodeTOB"
                  f"WHERE tpt.c_IdKomponenProduk in ({', '.join(data_produk)});")
+            if data_produk == [] :
+                data_produk_mix = []
+                if data_produk_mix ==[]:
+                    data_bundling = []
+                    if data_bundling == [] :
+                        query = (f"SELECT DISTINCT {', '.join(unique_select)}\n"
+                                f"FROM db_banksoalV2.t_TOB tt\n") 
     
                 
     
