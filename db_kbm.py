8b9bf1d2104c774d5fd782d54b15b0edd63ac881
abrahamgobimbel <abraham9haan.go@gmail.com>
2023-11-14 13:17:16 +0700

    .

--- db_kbm.py
+++ db_kbm.py
@@ -4,7 +4,10 @@ def query_select(nama_table) :
         query = (f"SELECT *\n"
                 f"FROM db_banksoalV2.t_BAH  tb \n"  
                 f"WHERE tb.c_TahunAjaran = '2023/2024'")
-        
+    
+    elif nama_table == 't_bah_kelas' :
+        query = (f"SELECT * FROM db_banksoalV2.t_BAHKelas tb")
+    
     elif nama_table == "t_cluster_pengajar" :
         query = (f"SELECT *\n"
                 f"from db_GOIconsV2.t_Cluster_Pengajar_iDPP tcpid ")
@@ -14,10 +17,20 @@ def query_select(nama_table) :
     elif nama_table == 't_feedback_pengajaran' :
         query = f"SELECT * FROM db_kbm.t_feedbackpengajaran WHERE c_LastUpdate >= DATE_SUB(CURDATE(), INTERVAL 2 WEEK) AND c_LastUpdate <= DATE_ADD(CURDATE(), INTERVAL 2 WEEK);"
     
+    elif nama_table == 't_isi_bah' :
+        query = (f"SELECT c_IdBAH , c_KodeBab , c_Pertemuan , c_Updater , c_LastUpdate  \n"
+                f"FROM db_banksoalV2.t_IsiBAH tib ")
+    
     elif nama_table == 't_kelas' :
         query = (f"select * \n"
                 f"from db_GOIconsV2.t_Kelas tk\n"
                 f"where c_TahunAjaran = '2023/2024'")
+        
+    elif nama_table == 't_kelas_siswa' :
+        query = (f"SELECT DISTINCT tk.c_IdKelas, tcs.c_NoRegistrasi, tk.c_Updater , tk.c_LastUpdate \n"
+                f"FROM db_GOIconsV2.t_Kelas tk \n"
+                f"JOIN db_GOIconsV2.t_Cluster_Siswa tcs USING (c_idkelas)\n"
+                f"WHERE tcs.c_TahunAjaran = '2023/2024'")
     
     elif nama_table == 't_permintaan_tst' :
         query = (f"SELECT *\n"
@@ -30,7 +43,9 @@ def query_select(nama_table) :
     elif nama_table == 't_rencana_kerja' :
         query = (f"SELECT *\n"
                 f"FROM db_IsianDPPV3.t_RencanaKerja trk \n" 
-                f"WHERE c_TahunAjaran = '2023/2024'")
+                f"WHERE c_TahunAjaran = '2023/2024'\n"
+                f"and c_JamAwal > '2023-01-01 00:00:00'")
+        
     elif nama_table == 't_rencana_kerja_kbm' :
         query = f"SELECT r.c_IdRencana, r.c_IdGedungPelaksana, r.c_NIK, r.c_JamAwal, r.c_JamAkhir, CASE WHEN CAST(r.c_idkegiatan AS SIGNED) = 20101 THEN 1 WHEN CAST(r.c_idkegiatan AS SIGNED) = 20102 THEN 2 WHEN CAST(r.c_idkegiatan AS SIGNED) = 20103 THEN 3 WHEN CAST(r.c_idkegiatan AS SIGNED) = 20104 THEN 4 WHEN CAST(r.c_idkegiatan AS SIGNED) = 20105 THEN 5 WHEN CAST(r.c_idkegiatan AS SIGNED) = 20106 THEN 6 WHEN CAST(r.c_idkegiatan AS SIGNED) = 20107 THEN 7 WHEN CAST(r.c_idkegiatan AS SIGNED) = 20108 THEN 8 WHEN CAST(r.c_idkegiatan AS SIGNED) = 20109 THEN 9 WHEN CAST(r.c_idkegiatan AS SIGNED) = 20110 THEN 10 WHEN CAST(r.c_idkegiatan AS SIGNED) = 20111 THEN 11 WHEN CAST(r.c_idkegiatan AS SIGNED) = 20112 THEN 12 WHEN CAST(r.c_idkegiatan AS SIGNED) = 20113 THEN 13 WHEN CAST(r.c_idkegiatan AS SIGNED) = 20114 THEN 14 WHEN CAST(r.c_idkegiatan AS SIGNED) = 20115 THEN 15 WHEN CAST(r.c_idkegiatan AS SIGNED) = 20116 THEN 16 WHEN CAST(r.c_idkegiatan AS SIGNED) = 20117 THEN 17 WHEN CAST(r.c_idkegiatan AS SIGNED) = 20118 THEN 18 WHEN CAST(r.c_idkegiatan AS SIGNED) = 20119 THEN 19 WHEN CAST(r.c_idkegiatan AS SIGNED) = 20120 THEN 20 WHEN CAST(r.c_idkegiatan AS SIGNED) = 20201 THEN 21 WHEN CAST(r.c_idkegiatan AS SIGNED) = 20202 THEN 22 WHEN CAST(r.c_idkegiatan AS SIGNED) = 20203 THEN 23 WHEN CAST(r.c_idkegiatan AS SIGNED) = 20205 THEN 25 WHEN CAST(r.c_idkegiatan AS SIGNED) = 20206 THEN 26 WHEN CAST(r.c_idkegiatan AS SIGNED) = 20207 THEN 27 ELSE 0 END AS c_idkegiatan, r.c_Info1 , r.c_Info2, r.c_Info3, r.c_TahunAjaran, r.c_StatusRencana, r.c_UpdaterRencana, r.c_LastUpdate, JSON_UNQUOTE(r.c_Keterangan->'$.RENCANA') AS c_rencana, JSON_UNQUOTE(r.c_Keterangan->'$.REALISASI') AS c_realisasi, JSON_UNQUOTE(r.c_Keterangan->'$.siswahadir') AS c_siswahadir FROM db_kbm.t_rencanakerja r WHERE (c_idkegiatan LIKE '0201%' OR c_idkegiatan LIKE '0202%') AND r.c_JamAwal >= DATE_SUB(CURDATE(), INTERVAL 2 WEEK) AND r.c_JamAwal <= DATE_ADD(CURDATE(), INTERVAL 2 WEEK);"
     return query
